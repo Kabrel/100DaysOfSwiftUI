8ce540089085e3a0380e4cfa39ce99adcc78d8f1
Kabrel <kshutov666@gmail.com>
2023-07-27 13:25:47 +0300

    added Word Scramble App

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Константин Шутов on 27.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
