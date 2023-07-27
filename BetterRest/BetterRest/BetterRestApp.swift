f5fda657cdf9c725f50f437252c6756f7bb1644c
Kabrel <kshutov666@gmail.com>
2023-07-27 13:04:13 +0300

    added Better Rest App

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Константин Шутов on 27.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
