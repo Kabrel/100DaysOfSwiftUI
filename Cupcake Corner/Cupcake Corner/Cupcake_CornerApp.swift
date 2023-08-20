8b29a40ab6ac95f0408a3eb1bba530e793d57e37
Kabrel <kshutov666@gmail.com>
2023-08-20 13:07:27 +0300

    added Cupcake App

---
+++ Cupcake Corner/Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Константин Шутов on 20.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
